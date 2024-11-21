5d44485e3d6eb12a02cf2583e8554371411e1ca1
EvanioTech <evanioshark@gmail.com>
2024-11-21 09:06:28 -0300

    adicionando funçao pra subir registros para a api

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,14 +1,61 @@
 import React, { useState } from "react";
 import { Background, Input, SubmitButton, SubmitText } from "./styles";
 import { Header } from "../../components/Header";
-import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard ,Alert} from "react-native";
 import { RegisterTypes } from "../../components/RegisterTypes";
+import api from "../../services/api";
+import { format } from 'date-fns' ;
+import { useNavigation } from "@react-navigation/native";
 
 
 const New = () => {
     const [motivo,setMotivo] = useState('');
     const [valor,setValor] = useState('');
     const [type,setType] = useState('receita');
+    const navigation = useNavigation();
+
+
+    function handleSubmit(){
+        Keyboard.dismiss();
+
+        if(isNaN(parseFloat(valor)) || type === null ){
+            alert('Preencha todos os campos')
+            return;
+        }
+
+        Alert.alert(
+            'confirmando dados',
+            `Tipo: ${type} - Valor: ${parseFloat(valor)}`,
+            [
+                {text: 'Cancelar',
+                    style: 'cancel'
+                },
+                {
+                    text: 'Continuar',
+                    onPress: () => handleAdd()
+                }
+            ]
+        )
+
+        
+
+    }
+    async function handleAdd() {
+        Keyboard.dismiss();
+
+        await api.post('/receive',{
+            description: motivo,
+            value: Number(valor),
+            type: type,
+            date: format(new Date(), 'dd/MM/yyyy')
+
+        })
+        setMotivo('');
+        setValor('');
+        navigation.navigate('Home');
+
+    }
+
     return(
         
 
@@ -30,7 +77,7 @@ const New = () => {
                 />
                 <RegisterTypes type={type} sendTypeChanged={(item) => setType(item) }/>
 
-                <SubmitButton onPress={()=>alert(motivo +' ' + valor)}>
+                <SubmitButton onPress={handleSubmit}>
                     <SubmitText>Registrar</SubmitText>
                 </SubmitButton>
             </SafeAreaView>
